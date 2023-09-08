6360e5f5c8f8b97d1c19ed7ef1ae531d396479ba
naoasa <asaumi1106n@gmail.com>
2023-09-09 00:20:58 +0900

    Finish 6.2.3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup # 各テストが走る直前に実行される
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid? # valid?メソッドで有効性を確認
+  end
+
+  test "name should be present" do # name属性が存ること
+    @user.name = "      "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do # email属性が存ること
+    @user.email = "      "
+    assert_not @user.valid?
+  end
+
+  test "name should be too long" do
+    @user.name = "a" * 51 # 51文字の文字列を作る
+    assert_not @user.valid? # 失敗、つまり有効性がないならtrue
+  end
+
+  test "email should be too long" do
+    @user.email = "a" * 244 + "@example.com" # 256文字の文字列を作る
+    assert_not @user.valid? # 失敗、つまり有効性がないならtrue
+  end
 end
