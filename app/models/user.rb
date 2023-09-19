a9520ee4b7ff69e62d20ae42383644a1dfefc52d
naoasa <asaumi1106n@gmail.com>
2023-09-19 12:56:42 +0900

    Finish Column 6.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     # emailの正規表現を定義(大文字で始まるものは定数)
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
-                                      format: { with: VALID_EMAIL_REGEX }
+                                      format: { with: VALID_EMAIL_REGEX },
+                                      uniqueness: { case_sensitive: false }
 end
