439da936f48d57a4d69ca4489f891f557f818b61
naoasa <asaumi1106n@gmail.com>
2023-09-20 14:06:29 +0900

    Finish 6.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    before_save { self.email = email.downcase } # save前に小文字に変換
+    before_save { email.downcase! } # save前に小文字に変換
     validates :name, presence: true, length: { maximum: 50 }
     # emailの正規表現を定義(大文字で始まるものは定数)
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
