8fc1fe2a00dd57e0f955b6f25ff53932dd2bfeca
naoasa <asaumi1106n@gmail.com>
2023-10-10 00:51:55 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation' # CSSのエラー表示部のid
     assert_select 'div.field_with_errors' # CSSのエラー表示部のclass
   end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
