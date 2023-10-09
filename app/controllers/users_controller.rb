4af4348eeef973d37156faac44879216ef5ed57d
naoasa <asaumi1106n@gmail.com>
2023-10-10 01:19:03 +0900

    Finish user signup and Ex.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # フラッシュメッセージ
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to user_url(@user)
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
