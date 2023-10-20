c828a75c19a4b0111e34c0e47771bc7e7b09f101
naoasa <asaumi1106n@gmail.com>
2023-10-20 14:10:33 +0900

    Before Ex. 8.2.6

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path # ログイン用のパスを開く
     assert_template 'sessions/new' # 新しいセッションのフォームが正しく表示されたことを確認
@@ -11,5 +15,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty? # フラッシュメッセージが表示されることを確認
     get root_path # 別のページにいったん移動
     assert flash.empty? # 移動先のページでフラッシュメッセージが表示されていないことを確認
+  end
+
+  test "login with valid information" do
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
