7dcdbfe8b4fc31aabeb673d19801bbdcd53c5a3b
naoasa <asaumi1106n@gmail.com>
2023-10-26 00:24:21 +0900

    Implement basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,52 +1,75 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest # UsersLoginを定義
 
   def setup
     @user = users(:michael)
   end
+end
 
-  test "login with valid email/invalid password" do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path, params: { session: { email:    @user.email,
-                                          password: "invalid" } }
-    assert_not is_logged_in? # ログインに成功していないことを確認
-    assert_response :unprocessable_entity
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
+class InvalidPasswordTest < UsersLogin # 誤りパスワードテストのクラス(UsersLoginを継承)
+
+  test "login path" do
+    get login_path # ログイン画面のGETリクエスト
+    assert_template 'sessions/new' # ログイン画面のビューを確認
   end
 
-  test "login with invalid information" do
-    get login_path # ログイン用のパスを開く
-    assert_template 'sessions/new' # 新しいセッションのフォームが正しく表示されたことを確認
-    post login_path, params: { session: { email: "", password: "" } } # わざと無効なparamsハッシュを使ってセッション用パスにPOSTする
-    assert_response :unprocessable_entity # 新しいセッションのフォームが正しいステータスを返すことを確認
-    assert_template 'sessions/new' # 新しいセッションのフォームが再度表示されることを確認
-    assert_not flash.empty? # フラッシュメッセージが表示されることを確認
-    get root_path # 別のページにいったん移動
-    assert flash.empty? # 移動先のページでフラッシュメッセージが表示されていないことを確認
-  end
-
-  test "login with valid information followed by logout" do
+  test "login with valid email/invalid password" do # 正しいメアド/誤りパスワード
     post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
-    assert is_logged_in? # ログイン成功の確認
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
-    delete logout_path # logoutのURLに対してDELETEリクエスト
+                                          password: "invalid"} }
     assert_not is_logged_in? # ログイン状態ではないことを確認
-    assert_response :see_other
-    assert_redirected_to root_url # ルートURLへの遷移を確認
-    follow_redirect!
-    assert_select "a[href=?]", login_path # ログインボタンの表示を確認
-    assert_select "a[href=?]", logout_path,      count: 0 # ログアウトボタンの非表示を確認
-    assert_select "a[href=?]", user_path(@user), count: 0 # プロフィールボタンの非表示を確認
+    assert_template 'sessions/new' # ログイン画面のビューを確認
+    assert_not flash.empty? # フラッシュメッセージが空ではないことを確認
+    get root_path # ルートURLへ移動
+    assert flash.empty? # フラッシュメッセージが空であることを確認
+  end
+end
+
+class ValidLogin < UsersLogin # 正しいログインのクラス(UsersLoginを継承)
+
+  def setup
+    super # UsersLoginクラスのメソッドを呼び出す
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password'} } # 正しいログイン情報をPOST
+  end
+end
+
+class ValidLoginTest < ValidLogin # 正しいログインテストのクラス(ValidLoginを継承)
+
+  test "valid login" do # 正しいログインのテスト
+    assert is_logged_in? # ログイン状態であることを確認
+    assert_redirected_to @user # ユーザー画面へのリダイレクトを確認
+  end
+
+  test "redirect after login" do
+    follow_redirect! # リダイレクト先に移動
+    assert_template 'users/show' # ユーザー画面のビューを確認
+    assert_select "a[href=?]", login_path, count: 0 # ログインリンクが見えない
+    assert_select "a[href=?]", logout_path # ログアウトリンクが見える
+    assert_select "a[href=?]", user_path(@user) # ユーザーリンクが見える
+  end
+end
+
+class Logout < ValidLogin # ログアウトクラスを定義(ValidLoginを継承)
+
+  def setup
+    super # ValidLoginクラスのメソッドを呼び出す
+    delete logout_path # ログアウトURLへDELETEリクエスト
+  end
+end
+
+class LogoutTest < Logout # ログアウトのテスト(Logoutを継承)
+
+  test "successful logout" do # ログアウトの成功自体をテスト
+    assert_not is_logged_in? # ログイン状態ではないことを確認
+    assert_response :see_other # 303 See Otherを確認
+    assert_redirected_to root_url # ルートURLへのリダイレクトを確認
+  end
+
+  test "redirect after logout" do # ログアウト後のリダイレクトのテスト
+    follow_redirect! # リダイレクト先に移動(ここからはログアウト後の世界)
+    assert_select "a[href=?]", login_path # ログインリンクが見える
+    assert_select "a[href=?]", logout_path,      count: 0 # ログアウトリンクが見えない
+    assert_select "a[href=?]", user_path(@user), count: 0 # ユーザーリンクが見えない
   end
 end
