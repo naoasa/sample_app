69d44e18723556309c9d9e05ec9e66e03bde5d3a
naoasa <asaumi1106n@gmail.com>
2023-11-01 16:37:26 +0900

    remember_digestカラムの追加と、rememberメソッドの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token # 外部からremember_tokenにアクセスできるようにする
     before_save { email.downcase! } # save前に小文字に変換
     validates :name, presence: true, length: { maximum: 50 }
     # emailの正規表現を定義(大文字で始まるものは定数)
@@ -14,5 +15,16 @@ class User < ApplicationRecord
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # 永続的セッションのためにユーザーをデータベースに記憶する
+    def remember # rememberメソッドを定義
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
     end
 end
