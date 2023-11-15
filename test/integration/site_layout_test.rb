db679352d5d6f4c0cff6802f5dbac20454a8b558
naoasa <asaumi1106n@gmail.com>
2023-11-16 01:41:23 +0900

    Finish 10.3.1

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,11 +9,32 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    get contact_path # '/contact'
-    # <title>full_title("Contact")</title>
-    assert_select "title", full_title("Contact")
-    # signupページのテスト
-    get signup_path # '/signup'
-    assert_select "title", full_title("Sign up")
+    assert_select "a[href=?]", login_path
+    get contact_path # '/contact'へアクセス
+    assert_select "title", full_title("Contact") # <title>full_title("Contact")</title>
+    get signup_path # '/signup'へアクセス
+    assert_select "title", full_title("Sign up") #<title>full_title("Sign up")</title>
+    get login_path # '/login'へアクセス
+    assert_select "title", full_title("Log in") #<title>full_title("Log in")</title>
+  end
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links when logged in" do
+    log_in_as(@user) # ヘルパーを用いてログイン
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path # ユーザー一覧へのリンク
+    assert_select "a[href=?]", user_path(@user) # プロフィールへのリンク
+    assert_select "a[href=?]", edit_user_path(@user) # プロフィール編集へのリンク
+    assert_select "a[href=?]", logout_path # ログアウトへのリンク
+    get users_path # "/users"へアクセス
+    assert_select "title", full_title("All users")
+    get user_path(@user) # "/users/:id"へアクセス
+    assert_select "title", full_title("#{@user.name}")
+    get edit_user_path(@user) # "/users/:id/edit"へアクセス
+    assert_select "title", full_title("Edit user")
   end
 end
