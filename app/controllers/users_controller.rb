8251c18f951c1cb4a5f336089a6b2183ac60fc0f
naoasa <asaumi1106n@gmail.com>
2023-11-20 14:00:39 +0900

    Finish 10.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
 
   def index
-    @users = User.all # 全ユーザーを@usersへ格納(あとでリファクタリングする)
+    @users = User.paginate(page: params[:page]) # ユーザー一覧(ページネーション対応)
   end
 
   def show
