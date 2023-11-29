a5d9255bf9f70fbad35c706323cc3a6946148edc
naoasa <asaumi1106n@gmail.com>
2023-11-30 00:46:12 +0900

    Finish 11.3.1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,16 +18,16 @@ module SessionsHelper
     # 記憶トークンcookieに対応するユーザーを返す(current_userメソッド)
     # 現在ログイン中のユーザーを返す(いる場合)
     def current_user
-        if (user_id = session[:user_id])
-          user = User.find_by(id: user_id)
-          @current_user ||= user if session[:session_token] == user.session_token
-        elsif (user_id = cookies.encrypted[:user_id])
-          user = User.find_by(id: user_id)
-          if user && user.authenticated?(:remember, cookies[:remember_token]) ##
-            log_in user
-            @current_user = user
-          end
+      if (user_id = session[:user_id])
+        user = User.find_by(id: user_id)
+        @current_user ||= user if session[:session_token] == user.session_token
+      elsif (user_id = cookies.encrypted[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(:remember, cookies[:remember_token])
+          log_in user
+          @current_user = user
         end
+      end
     end
 
     # 渡されたユーザーがカレントユーザーであればtrueを返す
