e1d2e8d137534a757fbf18ee7b10274ea1a16456
naoasa <asaumi1106n@gmail.com>
2023-12-01 21:38:43 +0900

    Finish Ex. 11.3-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,8 +52,7 @@ class User < ApplicationRecord
 
     # アカウントを有効にする(= 有効化属性を更新する)
     def activate
-        update_attribute(:activated,    true)
-        update_attribute(:activated_at, Time.zone.now)
+        update_columns(activated: true, activated_at: Time.zone.now)
     end
 
     # 有効化用のメールを送信する
