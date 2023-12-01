93428a441466aa5fe3d78d00db1f1fae8115884c
naoasa <asaumi1106n@gmail.com>
2023-12-01 21:21:37 +0900

    Before Ex. 11.3

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,8 +4,7 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email]) # メアドをもとにユーザーを探して格納
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated,    true)
-      user.update_attribute(:activated_at, Time.zone.now)
+      user.activate # アカウントを有効にするメソッド(Userモデルに定義)
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
