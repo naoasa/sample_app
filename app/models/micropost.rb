b9b8766e45c8956bbdbc27d3b19d86d02b12b91d
naoasa <asaumi1106n@gmail.com>
2023-12-07 01:04:25 +0900

    Finish 13.1.2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true # user_idが存在する
+  validates :content, presence: true, length: { maximum: 140 } # contentは存在し、最大文字数は140
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid? # ポストは有効であるか
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil # ポストと関連付けられたユーザーのidをnilにする
+    assert_not @micropost.valid? # ポストが有効ではないことを確認
+  end
+
+  test "content should be present" do # contentは存在する
+    @micropost.content = "   "
+    assert_not @micropost.valid? # ポストが有効ではないことを確認
+  end
+
+  test "content should be at most 140 characters" do # せいぜい140文字であることを確認
+    @micropost.content = "a" * 141 # aが141文字のポスト
+    assert_not @micropost.valid? # ポストが有効ではないことを確認
+  end
 end
