ac3558043c623179540fa0fb7a7671e902f7490f
naoasa <asaumi1106n@gmail.com>
2023-12-12 02:02:32 +0900

    Before List 13.44

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,21 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save # ポストが保存されたら
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home', status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def micropost_params # Strong Parameters
+      params.require(:micropost).permit(:content)
+    end
 end
