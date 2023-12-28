2d73b906867dce40ec4ac1b2bc5240027eb794a4
naoasa <asaumi1106n@gmail.com>
2023-12-28 12:57:37 +0900

    Before List 14.36

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,20 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user # アクションにはログイン済みであることが必須
 
   def create
-    user = User.find(params[:followed_id]) # _follow.html.erbより
-    current_user.follow(user) # followメソッドを実行
-    redirect_to user
+    @user = User.find(params[:followed_id]) # _follow.html.erbより
+    current_user.follow(@user) # followメソッドを実行
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.turbo_stream
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user) # unfollowメソッドを実行
-    redirect_to user, status: :see_other
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user) # unfollowメソッドを実行
+    respond_to do |format|
+      format.html { redirect_to @user, status: :see_other }
+      format.turbo_stream
+    end
   end
 end
